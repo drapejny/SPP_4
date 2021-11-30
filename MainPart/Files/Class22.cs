876f5ecb4a20ae760a801a76f906850468b6e9f5
Slizh Anton <antoshka-51762@yandex.by>
2021-11-30 22:31:51 +0300

    Add files with source code

---
+++ MainPart/Files/Class22.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Class21
+{
+    public IEnumerable<int> Interface { get; private set; }
+
+    public Class21(IDisposable s, ICloneable c, int a, string str) { }
+
+    public int Function1(int d, int e)
+    {
+        return 0;
+    }
+    public void Function2() { }
+}
+
+public class Class22
+{
+    public IEnumerable<int> Interface { get; private set; }
+
+    public void Function1() { }
+    public void Function2() { }
+}
+
+public class Class23
+{
+    public IEnumerable<int> Interface { get; private set; }
+
+    public void Function1() { }
+    public void Function2() { }
+}
+
+public class Class24
+{
+    public IEnumerable<int> Interface { get; private set; }
+
+    public void Function1() { }
+    public void Function2() { }
+}
+
+public class Class25
+{
+    public IEnumerable<int> Interface { get; private set; }
+
+    public void Function1() { }
+    public void Function2() { }
+}
+public class Class26
+{
+    public IEnumerable<int> Interface { get; private set; }
+
+    public void Function1() { }
+    public void Function2() { }
+}
---
+++ MainPart/Files/Class3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainPart.Files
+{
+    public class Class3
+    {
+
+        int t;
+        public Class3(string str)
+        {
+
+        }
+        public int met()
+        {
+            int b = 1;
+            return b;
+        }
+    }
+}
