a9a03180bfc1ce81bc0010a2cccaa43852dc3265
Slizh Anton <antoshka-51762@yandex.by>
2021-12-01 01:08:49 +0300

    Add files for tests

---
+++ UnitTestForLib/Files/TestFile1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestForLib
+{
+    public class TestClass1
+    {
+        public TestClass1() { }
+
+        public void met()
+        {
+            int a = 3;
+        }
+
+    }
+}
---
+++ UnitTestForLib/Files/TestFile2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestForLib.Files
+{
+    public class TestClass21
+    {
+        public void met() { }
+    }
+
+    public class TestClass22
+    {
+        public int met()
+        {
+            return 1;
+        }
+
+    }
+}
